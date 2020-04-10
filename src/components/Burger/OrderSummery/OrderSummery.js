8d8ea2840934ae32bb9566456966e79909a3ff66
samir taha <alrazy1@hotmail.com>
2020-04-10 22:44:33 +0300

    before routing

--- src/components/Burger/OrderSummery/OrderSummery.js
+++ src/components/Burger/OrderSummery/OrderSummery.js
@@ -4,7 +4,7 @@ import Button from "../../UI/Button/Button";
 
 class OrderSummary extends Component {
   componentDidUpdate() {
-    console.log('Order summary did update');
+    console.log('OrderSummary did update');
 }
 
   render(){
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -5,17 +5,17 @@ import Backdrop from "../Backdrop/Backdrop";
 
 class Modal extends Component {
   shouldComponentUpdate(nextProps, nextState) {
-    return nextProps.show !== this.props.show;
+    return nextProps.show !== this.props.show || nextProps.children !== this.props.children;
   }
 
   componentDidUpdate() {
-    console.log("modal will update");
+    console.log("modal did update");
   }
   render() {
     return (
       <Aux>
         <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
-        <div
+        <div 
           className={classes.Modal}
           style={{
             transform: this.props.show ? "translateY(0)" : "translateY(-100vh)",
